f534883737b7375b1e4012cb12d10127ddbe2bbf
Naoki Sekiguchi <web@likealunatic.jp>
2019-01-14 18:42:46 +0900

    Change to upload multiple files at once

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,6 +7,7 @@ function handleFileSelect (e) {
   if (!target) { return; }
   var files = target.files;
   if (!files) { return; }
+  var promises = [];
   for (var i = 0, l = files.length; i<l; i++) {
     var f = files[i];
     if (!f.type.match('image.*')) { continue; }
@@ -16,48 +17,43 @@ function handleFileSelect (e) {
     var formData = new FormData();
     formData.append('fileData', f);
     formData.append('fileLastModified', f.lastModified);
-
-    $.ajax('/create_image_note', {
-      method: 'POST',
-      data: formData,
-      contentType: false,
-      processData: false,
-      success: function (res) {
-        console.log(arguments);
-      },
-      error: function () {
-        console.log(arguments);
-      }
-    })
+    promises.push(promiseRequest(formData, i));
   }
+  return Promise.all(promises);
+}
+
+function promiseRequest(data, index) {
+  return new Promise((resolve, reject) => {
+    request(data, function (res) {
+      document.querySelectorAll('p.media')[index].classList.remove('media--uploading');
+      resolve();
+    }, function (err) {
+      console.error(err.message);
+      document.querySelectorAll('p.media')[index].classList.remove('media--uploading');
+      document.querySelectorAll('p.media')[index].classList.add('media--error');
+      reject();
+    });
+  });
 }
 
 function handleOnloadAsDataURL (theFile) {
   return function (e) {
     // Render thumbnail.
     var p = document.createElement('p');
+    p.className = "media media--uploading";
     p.innerHTML = ['<img class="thumb" src="', e.target.result,
       '" title="', escape(theFile.name), '"/>'].join('');
     document.getElementById('list').insertBefore(p, null);
   };
 }
-function handleOnloadAsArrayBuffer (theFile) {
-  return function (e) {
-    $.ajax('/create_image_note', {
-      dataType: 'json',
-      method: 'POST',
-      data: {
-        fileData: e.target.result,
-        fileName: theFile.name,
-        fileType: theFile.type,
-        fileSize: theFile.size
-      },
-      success: function (res) {
-        console.log(arguments);
-      },
-      error: function () {
-        console.log(arguments);
-      }
-    })
-  };
+
+function request (data, resolve, reject) {
+  $.ajax('/create_image_note', {
+    method: 'POST',
+    data: data,
+    contentType: false,
+    processData: false,
+    success: resolve,
+    error: reject
+  })
 }
