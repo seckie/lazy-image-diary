92cabd6891f69eb1d13deffd00474599a728a852
Naoki Sekiguchi <web@likealunatic.jp>
2019-09-23 22:43:45 +0900

    :lipstick: Apply new design

--- frontend/src/containers/CreateDiary.tsx
+++ frontend/src/containers/CreateDiary.tsx
@@ -1,62 +1,124 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {Dispatch} from 'redux';
-import classNames from 'classnames';
+import React from "react";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import classNames from "classnames";
 
-import actions from '../actions/';
-import { IFileData } from '../reducers/';
-import { UPLOAD_STATUS } from '../constants/';
+import actions from "../actions/";
+import { IFileData } from "../reducers/";
+import { UPLOAD_STATUS } from "../constants/";
 
-export function mapStateToProps (state: any) {
+export function mapStateToProps(state: any) {
   return state;
 }
 
-export function mapDispatchToProps (dispatch: Dispatch) {
+export function mapDispatchToProps(dispatch: Dispatch) {
   return {
     onChange: (e: React.FormEvent) => dispatch(actions.fileFieldOnChange(e))
   };
 }
 
 interface IUser {
-  name: string,
-  accessToken?: string,
-  user?: object
+  name: string;
+  accessToken?: string;
+  user?: object;
 }
 
 interface IProps {
-  fileDataset: IFileData[],
-  onChange: () => void
+  fileDataset: IFileData[];
+  resultMessages: string[];
+  errorMessages: string[];
+  onChange: () => void;
 }
 
-export const CreateDiary: React.FC<IProps> = (props) => {
+export const CreateDiary: React.FC<IProps> = props => {
   const tempUser: IUser = {
-    name: 'User A'
+    name: "User A"
   };
+
+  let emptyList = new Array(5).fill("");
+  emptyList = emptyList.map((item, i) => (
+    <li className="media media____empty" key={i} />
+  ));
+
   return (
-    <div className="App">
-      <h1>Upload images</h1>
-      <p>Evernote User Name: {tempUser.name}</p>
-      <p>
-        <input type="file" name="imagefiles[]" id="imagefiles" multiple accept=".jpg,.jpeg,.png,.gif" onChange={props.onChange} />
-      </p>
-      <div id="list">
-        {props.fileDataset.map((data: IFileData, i: number) => {
-          const mediaCName = classNames({
-            'media': true,
-            'media--uploading': data.status === UPLOAD_STATUS.uploading
-          });
-          return (
-            <p className={mediaCName} key={`media${i}`}>
-              <img className="thumb" src={data.path} title={global.escape(data.file.name)} alt={global.escape(data.file.name)} />
-            </p>
-          );
-        })}
+    <div className="app">
+      <header className="globalHeader">
+        <p className="logo">
+          <a href="/">Lazy Image Diary</a>
+        </p>
+        <p className="userInfo">Evernote ID: {tempUser.name}</p>
+      </header>
+      <div className="uploadUI1">
+        <p className="uploadInput">
+          <span className="inputFile">
+            <label htmlFor="imagefiles">画像ファイルを選択</label>
+            <input
+              type="file"
+              name="imagefiles[]"
+              id="imagefiles"
+              multiple
+              accept=".jpg,.jpeg,.png,.gif"
+              onChange={props.onChange}
+            />
+          </span>
+        </p>
+        <ul className="notes">
+          <li>選択できる画像ファイルは一度に20個まで </li>
+          <li>画像ファイルのサイズは一つ最大15MBまで</li>
+        </ul>
       </div>
-      <p>
-        <a href="/"> &lt;&lt; Back to index</a>
-      </p>
+      <div className="uploadUI2">
+        <p className="uploadInput">
+          <button className="btn">アップロード</button>
+        </p>
+        <ul className="messages">
+          {props.resultMessages.map(message => {
+            return (
+              <li className="messages__item">
+                <i className="fas fa-check"></i> {message}
+              </li>
+            );
+          })}
+          {props.errorMessages.map(message => {
+            return (
+              <li className="messages__item">
+                <i className="fas fa-times"></i> {message}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="mediaList">
+        <ul className="medias">
+          {(!props.fileDataset || !props.fileDataset[0]) && emptyList}
+          {props.fileDataset.map((data: IFileData, i: number) => {
+            const mediaCName = classNames({
+              media: true,
+              "media--uploading": data.status === UPLOAD_STATUS.uploading
+            });
+            return (
+              <li className={mediaCName} key={`media${i}`}>
+                <img
+                  className="thumb"
+                  src={data.path}
+                  title={global.escape(data.file.name)}
+                  alt={global.escape(data.file.name)}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(CreateDiary);
+CreateDiary.defaultProps = {
+  resultMessages: [],
+  errorMessages: []
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CreateDiary);
--- frontend/src/containers/Default.tsx
+++ frontend/src/containers/Default.tsx
@@ -1,35 +1,46 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {Dispatch} from 'redux';
+import React from "react";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
 
-import actions from '../actions/';
+import actions from "../actions/";
 
 export interface IProps {
-  onClickSignIn: () => void
+  onClickSignIn: () => void;
 }
 
 export function mapStateToProps(state: any) {
   return state;
 }
 
-export function mapDispatchToProps (dispatch: Dispatch) {
+export function mapDispatchToProps(dispatch: Dispatch) {
   return {
     onClickSignIn: () => dispatch(actions.signIn())
   };
 }
 
 export class Default extends React.Component<IProps> {
-  render () {
+  render() {
     return (
-      <div className="App">
-        <h1>Lazy Image Diary</h1>
-        <p><button className="btn btn-evernote-signin" onClick={this.props.onClickSignIn}>Evernote Sign-in</button></p>
-        <p>Lazy Image Diary is easy way to make diary on Evernote with your photos. <br />
-          | All you need is sign-in with your Evernote account and upload your photos from the upload form. <br />
-          | Then you would get auto generated posts with your photos on your Evernote "Diary" notebook.</p>
+      <div className="app">
+        <header className="globalHeader">
+          <h1 className="logo">Lazy Image Diary</h1>
+        </header>
+        <div className="lead">
+          <p>
+            <button
+              className="btn btn-evernote-signin"
+              onClick={this.props.onClickSignIn}
+            >
+              Evernote Sign-in
+            </button>
+          </p>
+        </div>
       </div>
     );
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Default);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Default);
