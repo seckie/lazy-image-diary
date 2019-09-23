555c79031281fb20a3d5f8f7524cf2331b8a155a
Naoki Sekiguchi <web@likealunatic.jp>
2019-09-23 10:42:47 +0900

    :wrench: Adjust upload configuration

--- backend/src/config/app-config.js
+++ backend/src/config/app-config.js
@@ -1,7 +1,7 @@
 const appConfig = {
-  SANDBOX: true, // change to false when you are ready to switch to production
+  SANDBOX: false, // change to false when you are ready to switch to production
   MAX_REQUEST_SIZE: 10000000,
-  MAX_FILE_SIZE: 10000000,
+  MAX_FILE_SIZE: 15000000,
   CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3001/oauth_callback',
   NOTEBOOK_NAME: 'Diary'
 };
