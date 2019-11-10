339200a357516ff4a9b3f367966ac1e3203912de
Naoki Sekiguchi <web@likealunatic.jp>
2019-11-10 23:16:45 +0900

    :white_check_mark: Fix test for Default component

--- frontend/src/__tests__/containers/Default.spec.tsx
+++ frontend/src/__tests__/containers/Default.spec.tsx
@@ -1,35 +1,42 @@
-import * as React from 'react';
-import { shallow } from 'enzyme';
-import { Default, mapStateToProps, mapDispatchToProps } from '../../containers/Default';
-import actions from '../../actions';
+import * as React from "react";
+import { shallow } from "enzyme";
+import {
+  Default,
+  mapStateToProps,
+  mapDispatchToProps
+} from "../../containers/Default";
+import actions from "../../actions";
+import { Lead } from "../../components/Lead";
 
-describe('Default', () => {
-  const props = {
-    onClickSignIn: jest.fn()
-  };
-  describe('mapStateToProps', () => {
+describe("Default", () => {
+  describe("mapStateToProps", () => {
     const state = {
-      test: 'test'
+      test: "test"
     };
-    it('just return state without mutation', () => {
+    it("just return state without mutation", () => {
       expect(mapStateToProps(state)).toEqual(state);
     });
   });
-  describe('mapDispatchToProps', () => {
+  describe("mapDispatchToProps", () => {
     const dispatch = jest.fn();
     const props = mapDispatchToProps(dispatch);
-    const RES = 'sign in response';
-    const signInSpy = jest.spyOn(actions, 'signIn').mockImplementation((): any => RES);
-    it('onClickSignIn() method calls dispatched signIn()', () => {
+    const RES = "sign in response";
+    const signInSpy = jest
+      .spyOn(actions, "signIn")
+      .mockImplementation((): any => RES);
+    it("onClickSignIn() method calls dispatched signIn()", () => {
       props.onClickSignIn();
       expect(signInSpy).toBeCalled();
       expect(dispatch).toBeCalledWith(RES);
     });
   });
-  describe('render', () => {
-    it('render sign-in button', () => {
+  describe("render", () => {
+    const props = {
+      onClickSignIn: jest.fn()
+    };
+    it('Lead component with "onClickSignIn" prop', () => {
       const el = shallow(<Default {...props} />);
-      expect(el.find('.btn-evernote-signin')).toHaveLength(1);
+      expect(el.find(Lead).prop("onClickSignIn")).toEqual(props.onClickSignIn);
     });
   });
 });
