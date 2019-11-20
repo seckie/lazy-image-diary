da8f3a84d20a790888051231982af0df48c83568
Naoki Sekiguchi <web@likealunatic.jp>
2019-11-21 00:51:28 +0900

    :sparkles: Init gh-pages

---
+++ storybook/precache-manifest.3cceb079af422a5ed256dfe38c0a0b7c.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264f584b472966f4d2da01449b5425e0",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.4b3a42349dde725f8670.bundle.js"
+  },
+  {
+    "revision": "0c3915c8a3fe55a739e8",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.4b3a42349dde725f8670.bundle.js"
+  },
+  {
+    "url": "vendors~main.4b3a42349dde725f8670.bundle.js"
+  }
+]);
