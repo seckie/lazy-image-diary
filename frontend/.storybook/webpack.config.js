65a5452623f0373d10145a5bfa1631520d44107b
Naoki Sekiguchi <likealunatic@gmail.com>
2022-02-13 23:52:14 +0900

    :wrench: Fix storybook config

--- frontend/.storybook/webpack.config.js
+++ frontend/.storybook/webpack.config.js
@@ -1,11 +1,7 @@
 module.exports = ({ config, mode }) => {
   config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    loader: require.resolve("babel-loader"),
-    options: {
-      presets: [["react-app", { flow: false, typescript: true }]]
-    }
+    test: /\.(ts|tsx)$/
   });
-  config.resolve.extensions.push(".ts", ".tsx");
+  config.resolve.extensions.push('.ts', '.tsx');
   return config;
 };
